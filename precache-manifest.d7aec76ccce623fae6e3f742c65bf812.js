5195aec1f36b4a32c6cc0d3a2c17dcdb2c864fa8
Rena kim <rena@wiznet.io>
2019-01-30 13:24:51 +0900

    Updates

---
+++ precache-manifest.d7aec76ccce623fae6e3f742c65bf812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chalsu/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2487c92ee7ecdf2f439c",
+    "url": "/chalsu/static/js/runtime~main.2487c92e.js"
+  },
+  {
+    "revision": "54b168c7098a64ba9730",
+    "url": "/chalsu/static/js/main.54b168c7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/chalsu/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "54b168c7098a64ba9730",
+    "url": "/chalsu/static/css/main.f790037e.chunk.css"
+  },
+  {
+    "revision": "16a1f159a16de375eb62a259ed1bfa2f",
+    "url": "/chalsu/index.html"
+  }
+];
